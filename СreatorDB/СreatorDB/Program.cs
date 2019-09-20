bd7cba80477b528334ba81a1543461d2a536833b
10tekinvo@gmail.com <10tekinvo@gmail.com>
2019-09-20 16:59:04 +0600

    Edit commit

--- СreatorDB/СreatorDB/Program.cs
+++ СreatorDB/СreatorDB/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Reflection;
+using System.Text;
 
 namespace СreatorDB
 {
@@ -15,29 +16,53 @@ namespace СreatorDB
             types.Add("System.String", "nvarchar(max)");
             types.Add("System.Guid", "uniqueidentifier");
             types.Add("System.DateTime", "date");
+            types.Add("System.Boolean", "BIT");
+            types.Add("System.Int64", "bigint");
+            types.Add("System.Byte", "byte");
 
             foreach (var type in assembly.GetTypes())
             {
-                if(type.Name == "Program")
+                if (type.Name == "Program")
                 {
                     break;
                 }
-                script += $"\nCREATE Table {type.Name}\n(\n  ";
+                script += $"\nCREATE Table {type.Name}\n(";
                 foreach (var memberInfo in type.GetMembers())
                 {
                     Console.WriteLine($"{memberInfo.Name} ----------- {memberInfo.GetType()} ");
 
                     if (memberInfo is PropertyInfo)
                     {
-                        script += $@"{memberInfo.Name} ";
+                        script += $"\n  {memberInfo.Name} ";
                         var propertyInfo = memberInfo as PropertyInfo;
-                        script += $"{types[propertyInfo.PropertyType.ToString()]}";
+                        if (!propertyInfo.PropertyType.ToString().Contains("Nullable"))
+                        {
+                            script += $"{types[propertyInfo.PropertyType.ToString()]} not null";
+                        }
+                        else
+                        {
+                            script += $"{types[DeleteNullable(propertyInfo.PropertyType.ToString())]}";
+                        }
+                        Console.WriteLine(script);
                         //Console.WriteLine(propertyInfo.PropertyType + "123123123123123123123");
                     }
                 }
             }
-            Console.WriteLine(script);
             Console.ReadKey();
+        }
+
+        public static string DeleteNullable(string text)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            var beginIndex = text.LastIndexOf('[');
+            var endIndex = text.LastIndexOf(']') - 1;
+            while (beginIndex < endIndex)
+            {
+                beginIndex++;
+                stringBuilder.Append(text[beginIndex]);
+            }
+            var txt = stringBuilder.ToString();
+            return txt;
         }
     }
 }
